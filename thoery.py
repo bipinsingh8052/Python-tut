b3fd270a9ff4cad70b753eefa52b36f018cbb0e0
Bipin Singh <bipinsingh8052@gmail.com>
2024-09-26 20:42:57 +0530

    first

---
+++ thoery.py
@@ -0,0 +1,33 @@
+# application: 
+# 1.webapplication
+# 2. ai/ms/ds
+# 3.scientific calculation
+# 4.automation
+
+# introduction of python
+# 
+
+
+# # feature of python
+# 1.it is high level lanuage.
+# 2.it is object oriented lang.
+
+
+
+
+print("hello world")
+print(89);
+# Escape sequences
+print("hello \"hello\" world")
+print('helllo \'hellow\' world')
+print("hello \\ world")
+print("jellow \n world");
+print("hellow \t wolrd")
+print("heelow\btext");
+# print('heelo \')
+# when you do it this in this python complier read a backspace in "" not a \ this give me error
+
+
+
+import calendar
+print(calendar.month(2002,8));
