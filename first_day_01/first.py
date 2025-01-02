442fb7ef4ad1e48d3e29f3ec6341edcf84a7420c
Bipin Singh <bipinsingh8052@gmail.com>
2025-01-02 18:24:44 +0530

    working

--- first_day_01/first.py
+++ first_day_01/first.py
@@ -1,1 +1,12 @@
 # how to python code run
+a=15; 
+b=6;
+print("sum of tum number:",a+b);
+print("sub of tum number:",a-b);
+print("multiple of tow number:",a*b);
+print("find the sqaure to one number",a**b);
+print("find the divide the two number:",a/b);
+print("find the not take a float number:",a//b);
+print("find the module of two no:",a%b);
+c=int(input("enter the nu:"))
+print(a+b+c) 
